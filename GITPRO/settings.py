e80bfa6913424eb1557879a252c409434946996e
sivakokkiligadda1985 <108756443+sivakokkiligadda1985@users.noreply.github.com>
2023-09-10 11:29:12 +0530

    Update settings.py

--- GITPRO/settings.py
+++ GITPRO/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7-y=_c#cu2ao3+l&ec$@9p$i(_d6p%#=5_e=f@mwqyf9_&5#g@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.234.111.78']
 
 
 # Application definition
