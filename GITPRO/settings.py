efc9d9100488ba46f869f38a157e901062d32a66
sivakokkiligadda1985 <108756443+sivakokkiligadda1985@users.noreply.github.com>
2023-09-10 12:39:33 +0530

    Update settings.py

--- GITPRO/settings.py
+++ GITPRO/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7-y=_c#cu2ao3+l&ec$@9p$i(_d6p%#=5_e=f@mwqyf9_&5#g@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['13.233.17.190']
 
